5480e803b4b232dfae580069f6d4c5c664caa264
Stela Capsa <stela.capsa@gmail.com>
2020-01-10 02:31:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,12 +11,12 @@ class Song
     @@all 
   end 
 
-  # def self.new_by_filename(filename)
-  #   artist_name, song_name, extra = filename.split(" - ")
-  #   song = self.new(song_name)
-  #   artist = Artist.find_or_create_by_name(artist_name)
-  #   artist.add_song(song)
-  # end
+  def self.new_by_filename(filename)
+    artist_name, song_name, extra = filename.split(" - ")
+    song = self.new(song_name)
+    artist = Artist.find_or_create_by_name(artist_name)
+    artist.add_song(song)
+  end
   
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
