52c66d435f97af6e980ea50734c2a8e07a8826ee
Stela Capsa <stela.capsa@gmail.com>
2020-01-10 02:21:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,4 +17,10 @@ class Song
     artist = Artist.find_or_create_by_name(artist_name)
     artist.add_song(song)
   end
+  
+  def artist_name=(name)
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist
+    artist.add_song(self)
+  end
 end
